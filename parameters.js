202d1b9bc50170c8cfe9f9e01f09c5b24099d9d8
Fumeng-Yang <yang.form@gmail.com>
2015-02-21 20:15:45 -0500

    fit to mobile phone...

--- parameters.js
+++ parameters.js
@@ -1,8 +1,17 @@
-var para = { 'menu_height': 70,
-    'head_width': 300,
-    'head_height': 70,
-    'screen_width': 300,
-    'screen_height': 470
+var para = { 'menu_height': screen.height * 0.5,
+    'menu_width': screen.width * 0.5 * 3,
+    'menu_2height': screen.height * 0.8,
+    'menu_2width': screen.width * 1.3,
+    'head_width': screen.width * 3.1,
+    'head_height': screen.height * 0.3,
+    'screen_width': screen.width * 3,
+    'screen_height': screen.height * 2.5,
+    'big' : '120px',
+    'small': '40px',
+    'mid': '60px',
+    'mid_margin' : screen.width * 0.2,
+    'top_margin' : screen.height * 0.5,
+    'top_margin_small' : screen.height * 0.1
     },
     //new Date(year, month, day, hours, minutes)
     data = [
