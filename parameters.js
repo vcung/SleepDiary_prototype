11c8024ef50ef5c4e423304a54fb97bd8c472912
Fumeng-Yang <yang.form@gmail.com>
2015-02-24 15:52:54 -0500

    data more data

--- parameters.js
+++ parameters.js
@@ -2,7 +2,7 @@ var para = {
     'fhead' : '25px',
     'fbig': '20px',
     'fmid': '18px',
-    'fsmall': '10px',
+    'fsmall': '8px',
     'layout': 'standard'
     },
     motto = [
@@ -16,24 +16,72 @@ var para = {
     'People who say they sleep like a baby usually don\'t have one. ~Leo J. Burke'
     ],
     data = [
-     {'Bedtime': {'Date':'02-15-2015', 'Time':'23:30'}, 
-     'Wakeup': {'Date':'02-16-2015', 'Time':'07:30'}, 
+     {'Bedtime': new Date(2015,2,15,23,30), 
+     'Wakeup': new Date(2015,2,16,08,30), 
      'Interrupt': ['00:10', '00:20'],
      'Rating': 4.3,
      'Comment': ''},
-
-     {'Bedtime': {'Date':'02-17-2015', 'Time':'00:30'}, 
-     'Wakeup':{'Date':'02-17-2015', 'Time':'08:30'}, 
+     {'Bedtime': new Date(2015,2,16,23,50), 
+     'Wakeup':new Date(2015,2,17,08,40), 
      'Interrupt': ['02:10'],
      'Rating': 4.2,
-     'Comment': 'I was plagued by nightmares'}
-    ],
-    current_entry = [
-    {'Bedtime': {'Date':'2015-02-22', 'Time':'22:30'}, 
-     'Wakeup': {'Date':'2015-02-23', 'Time':'07:30'}, 
+     'Comment': 'I was plagued by nightmares'},
+     {'Bedtime': new Date(2015,2,18,00,30), 
+     'Wakeup':new Date(2015,2,18,09,30), 
      'Interrupt': [],
      'Rating': 5,
-     'Comment': 'I was plagued by nightmares'}]
+     'Comment': ''},
+
+     {'Bedtime': new Date(2015,2,19,01,50), 
+     'Wakeup':new Date(2015,2,19,10,10), 
+     'Interrupt': [],
+     'Rating': 5,
+     'Comment': ''},
+
+     {'Bedtime': new Date(2015,2,19,23,55), 
+     'Wakeup':new Date(2015,2,20,09,20), 
+     'Interrupt': [],
+     'Rating': 4.8,
+     'Comment': 'milk'},
+
+     {'Bedtime': new Date(2015,2,20,23,20), 
+     'Wakeup':new Date(2015,2,21,07,40), 
+     'Interrupt': ['02:10', '04:50'],
+     'Rating': 3,
+     'Comment': 'feel tired'},
+
+      {'Bedtime': new Date(2015,2,21,01,40), 
+     'Wakeup':new Date(2015,2,21,06,10), 
+     'Interrupt': [],
+     'Rating': 2.1,
+     'Comment': 'dead'},
+
+      {'Bedtime': new Date(2015,2,21,23,15), 
+     'Wakeup':new Date(2015,2,22,08,10), 
+     'Interrupt': [],
+     'Rating': 3,
+     'Comment': 'dead'},
+
+     {'Bedtime': new Date(2015,2,22,23,10), 
+     'Wakeup':new Date(2015,2,23,08,10), 
+     'Interrupt': [],
+     'Rating': 4,
+     'Comment': ''},
+
+     {'Bedtime': new Date(2015,2,24,00,40), 
+     'Wakeup':new Date(2015,2,24,09,10), 
+     'Interrupt': [],
+     'Rating': 4.1,
+     'Comment': ''},
+
+     {'Bedtime': new Date(2015,2,25,23,50), 
+     'Wakeup':new Date(2015,2,26,08,20), 
+     'Interrupt': [],
+     'Rating': 5.3,
+     'Comment': ''}
+    ],
+    current_entry = [
+   ]
 
 //     //new Date(year, month, day, hours, minutes)
 //     data = [
@@ -55,6 +103,16 @@ var para = {
 //      ],
 //      current_entry = 'e2'
 
+function formatTime(d){
+    return d.getMonth() + '-' + d.getDate() + '-' + (d.getYear() + (2015 - 115)) 
+    + ' ' + d.getHours() + ':' + d.getMinutes();
+}
+
+function getHoursBetween (d1, d2) {
+
+    var tmp = Math.abs(d1.getTime() - d2.getTime())
+    return tmp / 36 /100000;
+}
 $(document).ready(function(){
 $("#back-save-custom").click(function(){
     $(".overlay").show();
