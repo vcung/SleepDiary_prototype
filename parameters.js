37ee920d641849dc6f9e23e3e79d0f5b7c517e7c
Fumeng-Yang <yang.form@gmail.com>
2015-02-24 00:35:28 -0500

    comment out some of parameters

--- parameters.js
+++ parameters.js
@@ -1,42 +1,41 @@
-/*var para = { 'menu_height': screen.height * 0.5,
-    'menu_width': screen.width * 0.5 * 3,
-    'menu_2height': screen.height * 0.8,
-    'menu_2width': screen.width * 1.3,
-    'head_width': screen.width * 3.1,
-    'head_height': screen.height * 0.3,
-    'screen_width': screen.width * 3,
-    'screen_height': screen.height * 2.5,
-    'big' : '120px',
-    'small': '40px',
-    'mid': '60px',
-    'mid_margin' : screen.width * 0.2,
-    'top_margin' : screen.height * 0.5,
-    'top_margin_small' : screen.height * 0.1
+var para = {
+    'fhead' : '25px',
+    'fbig': '20px',
+    'fmid': '18px',
+    'fsmall': '10px',
+    'layout': 'standard'
     },
-    //new Date(year, month, day, hours, minutes)
+    motto = [
+    'Sleeping is no mean art: for its sake one must stay awake all day. ~Friedrich Nietzsche',
+    'If you can\'t sleep, then get up and do something instead of lying there worrying. ~Dale Carnegie',
+    'No day is so bad it can\'t be fixed with a nap. ~Carrie Snow',
+    'Life is something that happens when you can\'t get to sleep. ~Fran Lebowitz',
+    'It\'s a cruel season that makes you get ready for bed while it\'s light out. ~Bill Watterson',
+    'A good laugh and a long sleep are the best cures in the doctor\'s book. ~Irish Proverb',
+    'Life is too short to sleep on low thread-count sheets. ~Leah Stussy',
+    'People who say they sleep like a baby usually don\'t have one. ~Leo J. Burke'
+    ]
 
-    data = [
-     'e1': {'Date1': new Date(2015,2,15),
-     'Date2': new Date(2015,2,16),
-     'BedTime': new Date(2015,2,15,23,30),
-     'Wakeup': new Date(2015,2,16,07,30),
-     'Interrupt': [new Date(2015,2,16,05,3)],
-     'Rating': 4,
-     'Comment': 'say something...'},
+//     //new Date(year, month, day, hours, minutes)
+//     data = [
+//      'e1': {'Date1': new Date(2015,2,15),
+//      'Date2': new Date(2015,2,16),
+//      'BedTime': new Date(2015,2,15,23,30),
+//      'Wakeup': new Date(2015,2,16,07,30),
+//      'Interrupt': [new Date(2015,2,16,05,3)],
+//      'Rating': 4,
+//      'Comment': 'say something...'},
 
-     'e2': {'Date1': new Date(2015,2,16),
-     'Date2': new Date(2015,2,17),
-     'BedTime': new Date(2015,2,16,23,40),
-     'Wakeup': new Date(2015,2,17,08,30),
-     'Interrupt': [],
-     'Rating': 4,
-     'Comment': 'say something...'},
-     ],
-
-     current_entry = 'e2'
+//      'e2': {'Date1': new Date(2015,2,16),
+//      'Date2': new Date(2015,2,17),
+//      'BedTime': new Date(2015,2,16,23,40),
+//      'Wakeup': new Date(2015,2,17,08,30),
+//      'Interrupt': [],
+//      'Rating': 4,
+//      'Comment': 'say something...'}
+//      ],
+//      current_entry = 'e2'
     ;
-    ;
-*/
 
 $(document).ready(function(){
 $("#back-save-custom").click(function(){
