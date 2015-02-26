18ed67796c869b7dd6545a719247fcccde3b8fb8
Fumeng-Yang <yang.form@gmail.com>
2015-02-25 22:34:32 -0500

    mv common code here

---
+++ common_code.js
@@ -0,0 +1,19 @@
+   d3.selectAll('.back-btn')
+   .on('click', function() {
+      d3.select('body').html('');
+
+      d3.html('2-main.html', function(err, d) {
+          if(err) console.log(err);
+          d3.select('body').node().appendChild(d);
+      });
+    });
+   
+   d3.selectAll('.navbar-brand')
+   .on('click', function() {
+      d3.select('body').html('');
+
+      d3.html('2-main.html', function(err, d) {
+          if(err) console.log(err);
+          d3.select('body').node().appendChild(d);
+      });
+    });
